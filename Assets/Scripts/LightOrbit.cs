5789893706de776e16fe7ef263a7e7c319ff5900
Minh Vu <minhd_vu@yahoo.com>
2020-06-12 13:22:46 -0400

    added orbiting light to resemble the sun

---
+++ Assets/Scripts/LightOrbit.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightOrbit : MonoBehaviour
+{
+    public Transform planet = null;
+    public float speed = 10f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(planet);
+        transform.RotateAround(planet.position, planet.up, speed * Time.deltaTime);
+    }
+}
