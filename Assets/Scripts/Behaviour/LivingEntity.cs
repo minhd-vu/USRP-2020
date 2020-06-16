38109a66151552d4888cf712b7141f44981c55be
Minh Vu <minhd_vu@yahoo.com>
2020-06-16 09:48:51 -0400

    before trying to add mating

--- Assets/Scripts/Behaviour/LivingEntity.cs
+++ Assets/Scripts/Behaviour/LivingEntity.cs
@@ -1,13 +1,13 @@
 ﻿using UnityEngine;
 
-public class LivingEntity : MonoBehaviour {
+public class LivingEntity : MonoBehaviour
+{
 
     public int colourMaterialIndex;
     public Species species;
     public Material material;
 
     public Coord coord;
-    //
     [HideInInspector]
     public int mapIndex;
     [HideInInspector]
@@ -15,26 +15,30 @@ public class LivingEntity : MonoBehaviour {
 
     protected bool dead;
 
-    public virtual void Init (Coord coord) {
+    public virtual void Init(Coord coord)
+    {
         this.coord = coord;
         transform.position = Environment.tileCentres[coord.x, coord.y];
 
         // Set material to the instance material
-        var meshRenderer = transform.GetComponentInChildren<MeshRenderer> ();
+        var meshRenderer = transform.GetComponentInChildren<MeshRenderer>();
         for (int i = 0; i < meshRenderer.sharedMaterials.Length; i++)
         {
-            if (meshRenderer.sharedMaterials[i] == material) {
+            if (meshRenderer.sharedMaterials[i] == material)
+            {
                 material = meshRenderer.materials[i];
                 break;
             }
         }
     }
 
-    protected virtual void Die (CauseOfDeath cause) {
-        if (!dead) {
+    protected virtual void Die(CauseOfDeath cause)
+    {
+        if (!dead)
+        {
             dead = true;
-            Environment.RegisterDeath (this);
-            Destroy (gameObject);
+            Environment.RegisterDeath(this);
+            Destroy(gameObject);
         }
     }
 }
